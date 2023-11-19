1af03d7f74e61bd7f0fa06180f2ab5587094f200
kainat999 <kainatbajwah999@gmail.com>
2023-11-19 20:48:44 +0000

    change functions into view classes

--- timetracking/tracker/urls.py
+++ timetracking/tracker/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import register, login_view, dashboard
+from .views import RegisterView, LoginView, DashboardView
 
 urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', login_view, name='login'),
-    path('dashboard/', dashboard, name='dashboard'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
 
--- timetracking/tracker/views.py
+++ timetracking/tracker/views.py
@@ -14,57 +14,86 @@ from rest_framework import status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import SlidingToken
 from datetime import timedelta
+from rest_framework.views import APIView
 
 
-@csrf_exempt
-@api_view(['POST', 'GET'])
-def register(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
 
-    if not username or not password:
-        return JsonResponse({'error': 'Please provide both username and password'}, status=400)
+        if not username or not password:
+            return JsonResponse({'error': 'Please provide both username and password'}, status=400)
 
-    user = User.objects.create_user(username=username, password=password)
-    Profile.objects.create(user=user)
+        user = User.objects.create_user(username=username, password=password)
+        Profile.objects.create(user=user)
 
-    return JsonResponse({'message': 'Registration successful'})
-
-
-
-
+        return JsonResponse({'message': 'Registration successful'})
 
 
 
 
 
+# def register(request):
+#     username = request.data.get('username')
+#     password = request.data.get('password')
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def login_view(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
+#     if not username or not password:
+#         return JsonResponse({'error': 'Please provide both username and password'}, status=400)
 
-    user = authenticate(request, username=username, password=password)
+#     user = User.objects.create_user(username=username, password=password)
+#     Profile.objects.create(user=user)
 
-    if user is not None:
-        token = SlidingToken.for_user(user)
-        return Response({'token': str(token), 'message': 'Login successful'})
-    else:
-        return JsonResponse({'error': 'Invalid credentials'}, status=401)
+#     return JsonResponse({'message': 'Registration successful'})
 
 
 
-@api_view(['GET', 'POST', 'DELETE'])
-@authentication_classes([JWTAuthentication])
-@permission_classes([IsAuthenticated])
 
 
-def dashboard(request):
-    if request.method == 'GET':
+
+
+
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            token = SlidingToken.for_user(user)
+            return Response({'token': str(token), 'message': 'Login successful'})
+        else:
+            return JsonResponse({'error': 'Invalid credentials'}, status=401)
+
+
+
+# def login_view(request):
+#     username = request.data.get('username')
+#     password = request.data.get('password')
+
+#     user = authenticate(request, username=username, password=password)
+
+#     if user is not None:
+#         token = SlidingToken.for_user(user)
+#         return Response({'token': str(token), 'message': 'Login successful'})
+#     else:
+#         return JsonResponse({'error': 'Invalid credentials'}, status=401)
+
+
+
+
+class DashboardView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    def get(self, request):
         user = request.user
         records = TrackerRecord.objects.filter(user=user).order_by('-start_time')
-        
+    
         elapsedTimeChunks = []
         for record in records:
             if record.end_time:
@@ -87,12 +116,52 @@ def dashboard(request):
         }
 
         return Response(response_data)
-
-    elif request.method == 'POST':
+        
+    def post(self, request):
         serializer = TrackerRecordSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+    
+    def delete(self, request):
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+# def dashboard(request):
+#     if request.method == 'GET':
+#         user = request.user
+#         records = TrackerRecord.objects.filter(user=user).order_by('-start_time')
+        
+        # elapsedTimeChunks = []
+        # for record in records:
+        #     if record.end_time:
+        #         elapsedTimeChunks.append(record.end_time - record.start_time)
+        
+        # serializer = TrackerRecordSerializer(records, many=True)
+
+
+
+        # user_data = {
+        #     'id': user.id,
+        #     'username': user.username,
+        # }
+
+
+        # response_data = {
+        #     'user': user_data,
+        #     'records': serializer.data,
+        #     'elapsedTimeChunks': [str(chunk) for chunk in elapsedTimeChunks],  
+        # }
+
+        # return Response(response_data)
+
+    # elif request.method == 'POST':
+        # serializer = TrackerRecordSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save(user=request.user)
+        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # elif request.method == 'DELETE':
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
